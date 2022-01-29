8bc52bd2ad040d68e121a21c9bff5fd6fb337f16
Dan Payne <dpayne5532@gmail.com>
2022-01-29 05:50:50 -0700

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Dan Payne on 1/29/22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
